28cf45a02369d706fa916b346cea026b831014d1
Jacob Pickron <jpickron30@gmail.com>
2022-11-10 21:05:45 -0700

    added a var of score to add to the score if the user got it correct

--- script.js
+++ script.js
@@ -34,40 +34,66 @@ var questions = [
   ];
   console.log(questions);
   
-  var time = 50
-  var i = 0
+  var time = 50;
+  var i = 0;
+  var score = 0;
+  let timerInterval;
+  var gameOver = document.getElementById('end-game');
+  var scoresSaved = document.getElementById('save-scores');
 
   var element = document.querySelector("#quiz-body");
   
   document.getElementById('play-again').onclick= function(){
 
       
-      setInterval( function(){
-            if (time >0){
+      timerInterval = setInterval( function(){
+            if (time > 0){
             time --;
             document.getElementById('timer').textContent = time
+            } else {
+              clearInterval(timerInterval);
             }
           }, 1000)
 
-      showQuestion()
+      showQuestion();
+      endGame();
   }
 
   document.getElementById('quiz-body').onclick = function(e){
     var text = e.target.innerText;
     if (text === questions[i].answer){
-        document.getElementById('result').textContent= "correct"
+        document.getElementById('result').textContent= "Correct"
+        document.getElementById('result').style.display = "block";
+        score ++
+        document.getElementById('correct-score').textContent = score
     }else {
-        document.getElementById('result').textContent= "wrong"
+        document.getElementById('result').textContent= "Wrong"
         time -=5
     }
     i++
-    showQuestion()
+    showQuestion();
+    endGame();
 
   }
 
-  function showQuestion(){
 
-    
+  function showQuestion(){
+    if (i >= 4) {
+      console.log("finished");
+      clearInterval(timerInterval);
+      // build your end screen
+      function endGame() {
+       var quizHead = document.getElementById('quiz-head');
+       var playAgain = document.getElementById('play-again');
+       gameOver.style.display = "block";
+       scoresSaved.style.display = "block";
+       element.style.display = "none";
+       quizHead.style.display = "none";
+       playAgain.style.display = "none";
+      }
+      endGame(); 
+      document.getElementById('save-score').classList = "";
+    }else {
       element.innerHTML=
       `<h2 class="quiz-question" id="question">
       ${questions[i].title} <br> 
@@ -79,6 +105,21 @@ var questions = [
           <button>${questions[i].choices[3]}</button>
       </ul>
       `
+    }
+
+    
+      // element.innerHTML=
+      // `<h2 class="quiz-question" id="question">
+      // ${questions[i].title} <br> 
+      // </h2>
+      // <ul class="answer-choices">
+      //     <button >${questions[i].choices[0]}</button>
+      //     <button>${questions[i].choices[1]}</button>
+      //     <button>${questions[i].choices[2]}</button>
+      //     <button>${questions[i].choices[3]}</button>
+      // </ul>
+      // `
+
   }
 
   
